f45e32f04a7e26f83ecb0155cb61e4fbec45b559
Humberto Villarino <beto984@gmail.com>
2023-02-04 18:34:00 +0100

    Created custom methods for tag reading for each sphere

--- 2023 MoveWell-game/Assets/Scripts/EventInterceptor.cs
+++ 2023 MoveWell-game/Assets/Scripts/EventInterceptor.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class EventInterceptor : MonoBehaviour
 {
     Dictionary<string, int> positionsMap = new Dictionary<string, int>();
+    string[] currentSpheresPositions = new string[5];
 
     public SmartToyEventManager EV_A;
     public SmartToyEventManager EV_B;
     public SmartToyEventManager EV_C;
     public SmartToyEventManager EV_D;
     public SmartToyEventManager EV_E;
-    public DisplayManager disManager;
+    [SerializeField] public DisplayManager disManager;
     bool init = true;
-    string[] currentSpheresPositions = new string[5];
     public string[] solution;
 
     // Start is called before the first frame update
     void Start()
     {
         positionsMap.Add("A", 1);
+        //positionsMap.Add("B", 2);
+        //positionsMap.Add("C", 3);
+        //positionsMap.Add("D", 4);
+        //positionsMap.Add("E", 5);
     }
 
     // Update is called once per frame
@@ -27,14 +31,14 @@ public class EventInterceptor : MonoBehaviour
     {
         if (init)
         {
-            EV_A.readRFID.AddListener(ReadTagE);
-            EV_A.releaseRFID.AddListener(() => ReleaseTagE());
-            EV_B.readRFID.AddListener(ReadTagE);
-            EV_B.releaseRFID.AddListener(() => ReleaseTagE());
-            EV_C.readRFID.AddListener(ReadTagE);
-            EV_C.releaseRFID.AddListener(() => ReleaseTagE());
-            EV_D.readRFID.AddListener(ReadTagE);
-            EV_D.releaseRFID.AddListener(() => ReleaseTagE());
+            EV_A.readRFID.AddListener(ReadTagA);
+            EV_A.releaseRFID.AddListener(() => ReleaseTagA());
+            EV_B.readRFID.AddListener(ReadTagB);
+            EV_B.releaseRFID.AddListener(() => ReleaseTagB());
+            EV_C.readRFID.AddListener(ReadTagC);
+            EV_C.releaseRFID.AddListener(() => ReleaseTagC());
+            EV_D.readRFID.AddListener(ReadTagD);
+            EV_D.releaseRFID.AddListener(() => ReleaseTagD());
             EV_E.readRFID.AddListener(ReadTagE);
             EV_E.releaseRFID.AddListener(() => ReleaseTagE());
             init = false;
@@ -48,12 +52,51 @@ public class EventInterceptor : MonoBehaviour
         }
     }
 
+    public void ReadTagA(string tag)
+    {
+        Debug.Log("Hello Red");
+        currentSpheresPositions[positionsMap[tag]] = "R";
+    }
+
+    public void ReleaseTagA()
+    {
+        Debug.Log("Bye Red");
+        removeSphere("R");
+    }
+    
+    public void ReadTagB(string tag)
+    {
+        Debug.Log("Hello Pink");
+        currentSpheresPositions[positionsMap[tag]] = "P";
+    }
+
+    public void ReleaseTagB()
+    {
+        Debug.Log("Bye Pink");
+        removeSphere("P");
+    }
+    
+    public void ReadTagC(string tag)
+    {
+        Debug.Log("Hello Green");
+        currentSpheresPositions[positionsMap[tag]] = "G";
+    }
+
+    public void ReleaseTagC()
+    {
+        Debug.Log("Bye Green");
+        removeSphere("G");
+    }
+
     public void ReadTagD(string tag) {
-        Debug.Log("HI");
+        Debug.Log("Hello Yellow");
+        currentSpheresPositions[positionsMap[tag]] = "Y";
+
     }
     public void ReleaseTagD()
     {
-        Debug.Log("HI released");
+        Debug.Log("Bye Yellow");
+        removeSphere("Y");
     }
 
     public void ReadTagE(string tag)
@@ -65,14 +108,19 @@ public class EventInterceptor : MonoBehaviour
     {
         Debug.Log("HI released");
         Debug.Log("bye blue");
-        for( int i =0; i < currentSpheresPositions.Length; i++)
+        removeSphere("B");
+            
+    }
+
+    private void removeSphere(string sphere)
+    {
+        for (int i = 0; i < currentSpheresPositions.Length; i++)
         {
-            if (currentSpheresPositions[i] == "B")
+            if (currentSpheresPositions[i] == sphere)
             {
                 currentSpheresPositions[i] = "";
                 break;
             }
         }
-            
     }
 }
